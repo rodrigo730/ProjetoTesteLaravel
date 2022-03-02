75ac34c86e73d3c62c167364112d9a101babc5fb
rodrigo730 <85645082+rodrigo730@users.noreply.github.com>
2022-03-01 21:20:39 -0300

    Atualizações

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -1,20 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<style>
-    h1{
-        text-align:center;
-    }
-</style>
-<body>
+@extends('layouts.main')
+
+@section('title' , 'Produtos')
+
+@section('content')
+
  <h1>Produtos<h1>
     <a href="/">Voltar Para a Home</a>
        <br/>
     <a href="/contact">Ir Para os Contatos</a>
-</body>
-</html>
+
+    @endsection
